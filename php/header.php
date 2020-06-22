3b1a78bf0253a0a5b07bf145b31501ba9747ef6f
SMorokhin <61113573+SMorokhin@users.noreply.github.com>
2020-06-22 15:21:22 +0500

    Add files via upload
    регистрация + авторизация

---
+++ php/header.php
@@ -0,0 +1,104 @@
+<?php
+    //Запускаем сессию
+    //session_start();
+?>
+ 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Название нашего сайта</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="css/styles.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <script type="text/javascript">
+            $(document).ready(function(){
+                "use strict";
+                //================ Проверка email ==================
+         
+                //регулярное выражение для проверки email
+                var pattern = /^[a-z0-9][a-z0-9\._-]*[a-z0-9]*@([a-z0-9]+([a-z0-9-]*[a-z0-9]+)*\.)+[a-z]+/i;
+                var mail = $('input[name=email]');
+                 
+                mail.blur(function(){
+                    if(mail.val() != ''){
+         
+                        // Проверяем, если введенный email соответствует регулярному выражению
+                        if(mail.val().search(pattern) == 0){
+                            // Убираем сообщение об ошибке
+                            $('#valid_email_message').text('');
+         
+                            //Активируем кнопку отправки
+                            $('input[type=submit]').attr('disabled', false);
+                        }else{
+                            //Выводим сообщение об ошибке
+                            $('#valid_email_message').text('Не правильный Email');
+         
+                            // Дезактивируем кнопку отправки
+                            $('input[type=submit]').attr('disabled', true);
+                        }
+                    }else{
+                        $('#valid_email_message').text('Введите Ваш email');
+                    }
+                });
+         
+                //================ Проверка длины пароля ==================
+                var password = $('input[name=password]');
+                 
+                password.blur(function(){
+                    if(password.val() != ''){
+         
+                        //Если длина введенного пароля меньше шести символов, то выводим сообщение об ошибке
+                        if(password.val().length < 6){
+                            //Выводим сообщение об ошибке
+                            $('#valid_password_message').text('Минимальная длина пароля 6 символов');
+         
+                            // Дезактивируем кнопку отправки
+                            $('input[type=submit]').attr('disabled', true);
+                             
+                        }else{
+                            // Убираем сообщение об ошибке
+                            $('#valid_password_message').text('');
+         
+                            //Активируем кнопку отправки
+                            $('input[type=submit]').attr('disabled', false);
+                        }
+                    }else{
+                        $('#valid_password_message').text('Введите пароль');
+                    }
+                });
+            });
+        </script>
+    </head>
+    <body>
+ 
+        <div id="header">
+            <h2>Шапка сайта</h2>
+ 
+            <a href="/index.php">Главная</a>
+ 
+            <div id="auth_block">
+            <?php
+                //Проверяем авторизован ли пользователь
+                if(!isset($_SESSION['email']) && !isset($_SESSION['password'])){
+                    // если нет, то выводим блок с ссылками на страницу регистрации и авторизации
+            ?>
+                    <div id="link_register">
+                        <a href="/form_register.php">Регистрация</a>
+                    </div>
+             
+                    <div id="link_auth">
+                        <a href="/form_auth.php">Авторизация</a>
+                    </div>
+            <?php
+                }else{
+                    //Если пользователь авторизован, то выводим ссылку Выход
+            ?> 
+                    <div id="link_logout">
+                        <a href="/logout.php">Выход</a>
+                    </div>
+            <?php
+                }
+            ?>
+            </div>
+             <div class="clear"></div>
+        </div>
